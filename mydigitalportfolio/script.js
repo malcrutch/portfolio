de9303aa6b06e8353e79bac84505d1869095513a
malcrutch <37163286+malcrutch@users.noreply.github.com>
2020-06-16 19:54:00 -0400

    Add files via upload

---
+++ mydigitalportfolio/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    
+$("#header").attr("style", "color: blue",).css("text-align", "center").css("font-size", "50px");
+$("#container").css("text-align", "auto");
+$("#contactMe").attr("style", "background-color: blue",);
+$("#returnBtnDiv").css("text-align", "center");
+$("#returnBtn").on("click", function(){
+    window.location.replace('file:///Users/malcolmcrutchfield/Desktop/mydigitalportfolio/index.html');
+ 
+
+});
+
+
+    
+});
